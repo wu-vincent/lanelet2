2e457eea57d75faea645691149132d565c51316d
Vincent <magicdroidx@gmail.com>
2023-11-19 22:59:47 +0000

    feat: create conan package for lanelet2

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,32 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
 class Lanelet2Conan(ConanFile):
+    name = "lanelet2"
+    version = "1.2.1"
+
+    # Optional metadata
+    license = "BSD"
+    url = "https://github.com/fzi-forschungszentrum-informatik/lanelet2"
+    description = "Map handling framework for automated driving"
+
+    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    proj_list = [
+        'lanelet2_core',
+        # 'lanelet2_io',
+        # 'lanelet2_matching',
+        # 'lanelet2_projection',
+        # 'lanelet2_traffic_rules',
+        # 'lanelet2_routing',
+        # 'lanelet2_validation'
+    ]
+
+    exports_sources = ["CMakeLists.txt"] + [f"{proj}/*" for proj in proj_list]
 
     def requirements(self):
         self.requires("boost/[>=1.75.0 <=1.81.0]")
@@ -13,5 +36,42 @@ class Lanelet2Conan(ConanFile):
         self.requires("gtest/1.14.0")
 
     def build_requirements(self):
-        # self.tool_requires("cmake/3.22.6")
         pass
+        # self.tool_requires("cmake/3.27.7")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = list(reversed([
+            'lanelet2_core',
+            # 'lanelet2_io',
+            # 'lanelet2_matching',
+            # 'lanelet2_projection',
+            # 'lanelet2_traffic_rules',
+            # 'lanelet2_routing',
+            # 'lanelet2_validation'
+        ]))
