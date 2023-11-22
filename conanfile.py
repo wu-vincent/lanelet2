45ba2ec8fd239b3085a80f0f8777710fcc884f40
Vincent <magicdroidx@gmail.com>
2023-11-22 08:38:23 +0000

    feat: do not force the use of ninja on Windows

--- conanfile.py
+++ conanfile.py
@@ -60,7 +60,7 @@ class Lanelet2Conan(ConanFile):
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
-        tc = CMakeToolchain(self, generator="Ninja")
+        tc = CMakeToolchain(self)
         tc.generate()
 
     def build(self):
